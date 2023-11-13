efb603b636abc73620d9d30eb293c17575ade50b
Sofia Protsiv <prochivsofia@gmail.com>
2023-11-13 12:36:52 +0200

    feat: add 06 hw solution

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,22 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum Days {
+  MONDAY,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY,
+  SUNDAY,
+}
+function isWeekend(day: Days): boolean {
+  return day === Days.SATURDAY || day === Days.SUNDAY;
+}
+
+if (isWeekend(Days.WEDNESDAY)) {
+  console.log("Today is a day off");
+} else {
+  console.log("Today is a working day");
+}
--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,11 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<K, V> {
+  key: K;
+  value: V;
 }
+
+const pair: KeyValuePair<number, string> = { key: 1, value: "one" };
 
 export {};
